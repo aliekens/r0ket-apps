4d0413b0abeee63ec04f208df24c44aab51c63b1
Mu <manu@heirbaut.com>
2011-08-11 18:34:34 +0200

    added sinus scroll

--- aScroll.c
+++ aScroll.c
@@ -8,6 +8,7 @@
 
 #include "usetable.h"
 
+/*
 void main_walle(void) {
   int blink_it = 0;
   int sine_len = 100;
@@ -58,29 +59,43 @@ void main_walle(void) {
     delayms(10);
     lcdFill(0); //clear screen
   }
+*/
+
+static int SINTABLE[100]={0,0,1,2,3,4,5,6,7,8,9,10,11,11,12,13,14,15,16,16,17,18,19,19,20,21,21,22,23,23,24,24,25,25,26,26,27,27,27,28,28,28,29,29,29,29,29,29,29,29,30,29,29,29,29,29,29,29,29,28,28,28,27,27,27,26,26,25,25,24,24,23,23,22,21,21,20,19,19,18,17,16,16,15,14,13,12,11,11,10,9,8,7,6,5,4,3,2,1,0}; 
 
 void ram(void) {
 	int NICK_LEN = 0;
 	int x = 0;
 	int direction = 1; // 0 = right, 1 = left
+	int y_offset=0; 
+	char * nick = GLOBAL(nickname);
 
     lcdClear();
 	setExtFont(GLOBAL(nickfont));
-	NICK_LEN = DoString(0,12,GLOBAL(nickname));
+	NICK_LEN = DoString(0,12, nick);
 	x = -NICK_LEN;
-	lcdClear();
 	
 	while (1) {
-		DoString(x,12,GLOBAL(nickname));
+		//DoString(x,12,GLOBAL(nickname));
+		lcdClear();
+
+		int width = x;
+		for (int i = 0; i < strlen(nick); i++) {
+			char c = nick[i];
+			width = DoChar(width,  (96-getFontHeight())/2-SINTABLE[ (y_offset+i*8)%100] , c );			
+		}
+
+	    lcdDisplay();
 		
 		if (direction) x--; else x++;
 		if (x > 96)
 			x = -NICK_LEN;
 		if (x < -NICK_LEN)
 			x = 96;
-			
-	    lcdDisplay();
 
+		y_offset++;
+	    y_offset=y_offset%100;
+			
 		if (x%2 == 0) gpioSetValue (RB_LED0, 1-gpioGetValue(RB_LED0)); 
 	    if (x%2 == 1) gpioSetValue (RB_LED2, 1-gpioGetValue(RB_LED2)); 
 		
@@ -100,6 +115,6 @@ void ram(void) {
 		case BTN_DOWN:
 			break;
 		}
-	    delayms_queue_plus(15,0);
+	    delayms_queue_plus(5,0);
 	}
 }
